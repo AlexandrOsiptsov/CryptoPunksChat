676accf1d25c76e9e48eae52251ae17fd865bc73
AlexandrOsiptsov <osiptsov.alexandr.h@gmail.com>
2023-12-19 21:11:11 +0300

    Добавил в .env SECRET_KEY

--- chatapp/__init__.py
+++ chatapp/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
-
+import os
+from dotenv import load_dotenv
 from .routes import main
 from .events import socketio
 
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 def create_app():
     app = Flask(__name__, static_folder="static")
-    app.config["SECRET_KEY"] = "secret"
+    app.config["SECRET_KEY"] = SECRET_KEY
 
     app.register_blueprint(main)
 
